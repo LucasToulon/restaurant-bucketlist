2aefff54ee2d0a5fe95517d0db11d659e01bd61b
LucasToulon <lucas.toulon@yahoo.de>
2023-12-30 12:05:37 +0100

    Created project with spring initializer

---
+++ src/main/java/de/lucastoulon/restaurantbucketlist/RestaurantBucketlistApplication.java
@@ -0,0 +1,13 @@
+package de.lucastoulon.restaurantbucketlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantBucketlistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantBucketlistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/lucastoulon/restaurantbucketlist/RestaurantBucketlistApplicationTests.java
@@ -0,0 +1,13 @@
+package de.lucastoulon.restaurantbucketlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantBucketlistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
